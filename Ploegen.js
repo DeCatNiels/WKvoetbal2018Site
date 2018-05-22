2249f03d989f53f6b70ca0fe1642bb509a272a59
Robin <robin.braibant@student.pxl.be>
2018-05-22 20:42:29 +0200

    Ploegen deel2

--- Ploegen.js
+++ Ploegen.js
@@ -2,15 +2,30 @@ let naamIngave = document.getElementById('naam');
 let shortCode = document.getElementById('shortcode');
 let beschrijving = document.getElementById('beschrijving');
 let instelKnop = document.getElementById('toevoegButton');
+let landen = document.getElementById("landen");
 
 instelKnop.addEventListener('click', function(){
     let naam = naamIngave.value;
     let code = shortCode.value;
     let description = beschrijving.value;
+    landen.style.display = "block";
+
+    let vlagLand = document.createElement('img');
+    vlagLand.src = "afbeeldingen/landen/" + code + ".png";
 
     let element = document.createElement('div');
-    let para = element.creaElement('p');
-    para.value = naam;
+    element.id = "land";
+
+    let para = document.createElement('p');
+    para.innerHTML = naam;
+
+    let desc = document.createElement('p');
+    desc.id = "beschrijvingOutput";
+    desc.innerHTML = description;
+    landen.appendChild(element);
+    element.appendChild(vlagLand);
+    element.appendChild(para);
+    element.appendChild(desc);
 
 
-})
+});
