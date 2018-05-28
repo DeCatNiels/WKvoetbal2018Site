61d6f4cfe8aed837e6f68c58f21fbcfe1758fe66
Niels DeCat <11702349@student.pxl.be>
2018-05-28 11:54:28 +0200

    stadions show on map

--- stadions/navigation.js
+++ stadions/navigation.js
@@ -1,3 +1,9 @@
+/*
+var imported = document.createElement('script');
+imported.src = 'stadions.js';
+document.head.appendChild(imported);
+*/
+
 getLocation();
 var position;
 
@@ -13,13 +19,39 @@ function getLocation() {
     }
 }
 
-
-function myMap() {
+/*function myMap() {
     //var mapCanvas = document.
     var mapOptions = {
         center: new google.maps.LatLng(50.938241299999994, 5.347872),
-        zoom:16,
+        zoom:5,
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById("googleMaps"), mapOptions);
+}*/
+
+function myMap() {
+    var mapCanvas = document.getElementById("googleMaps");
+    var myCenter = new google.maps.LatLng(stadions[0].lat, stadions[0].long);
+    var mapOptions = {
+        center: myCenter,
+        zoom:5,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        animation: google.maps.Animation.bounce
+    });
+    marker.setMap(map);
+    addMarkers(map);
+}
+
+function addMarkers(map){
+    for(var i =0 ; i < stadions.length; i++){
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(stadions[i].lat, stadions[i].long),
+            animation: google.maps.Animation.bounce
+        });
+        marker.setMap(map);
+    }
 }
