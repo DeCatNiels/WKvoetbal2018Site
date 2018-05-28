14db9278f37da774e2beb51f4eb811a8c1e61dac
Robin <robin.braibant@student.pxl.be>
2018-05-28 11:51:04 +0200

    Ploegen klaar

--- javascript/Ploegen.js
+++ javascript/Ploegen.js
@@ -1,38 +1,51 @@
 let naamIngave = document.getElementById('naam');
 let shortCode = document.getElementById('shortcode');
 let beschrijving = document.getElementById('beschrijving');
+
 let instelKnop = document.getElementById('toevoegButton');
 let landen = document.getElementById("landen");
-let divs = [];
 
-console.log(JSON.parse(localStorage.getItem("saveLanden")));
+let laad = JSON.parse(localStorage.getItem("savedLanden"));
+let divs;
+
+if (laad === null){
+    divs = [];
+} else {
+    divs = laad;
+    laadLanden();
+}
 
 instelKnop.addEventListener('click', function(){
-    let naam = naamIngave.value;
-    let code = shortCode.value;
-    let description = beschrijving.value;
-    landen.style.display = "block";
-
-    let vlagLand = document.createElement('img');
-    vlagLand.src = "afbeeldingen/landen/" + code + ".png";
-
-    let element = document.createElement('div');
-    element.id = "land";
-
-    let para = document.createElement('p');
-    para.innerHTML = naam;
-
-    let desc = document.createElement('p');
-    desc.id = "beschrijvingOutput";
-    desc.innerHTML = description;
-    landen.appendChild(element);
-    element.appendChild(vlagLand);
-    element.appendChild(para);
-    element.appendChild(desc);
-
-    let land = {nm: naam, cd: code, besc: description};
+    let land = {naam: naamIngave.value, code: shortCode.value, beschr: beschrijving.value};
     divs.id = "opgeslagenLanden";
     divs.push(land);
+    laadLanden();
+    localStorage.setItem("savedLanden", JSON.stringify(divs));
 });
 
-localStorage.setItem("saveLanden",JSON.stringify(document.getElementById("opgeslagenLanden")));
+function laadLanden(){
+    landen.innerHTML = "";
+
+    for(let i = 0; i < divs.length; i++){
+        landen.style.display = "block";
+
+        let vlagLand = document.createElement('img');
+        vlagLand.src = "afbeeldingen/landen/" + divs[i].code + ".png";
+
+        let element = document.createElement('div');
+        element.id = "land";
+
+        let para = document.createElement('p');
+        para.innerHTML = divs[i].naam;
+
+        let desc = document.createElement('p');
+        desc.id = "beschrijvingOutput";
+        desc.innerHTML = divs[i].beschr;
+        landen.appendChild(element);
+        element.appendChild(vlagLand);
+        element.appendChild(para);
+        element.appendChild(desc);
+    }
+}
+
+
