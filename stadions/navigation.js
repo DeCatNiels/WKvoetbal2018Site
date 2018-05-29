2d55da74091fdcc585f47e5a0335bfca1db32287
Niels DeCat <11702349@student.pxl.be>
2018-05-29 17:17:59 +0200

    functionaliteit stadions af

--- stadions/navigation.js
+++ stadions/navigation.js
@@ -38,20 +38,37 @@ function myMap() {
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({
-        position: myCenter,
-        animation: google.maps.Animation.bounce
-    });
-    marker.setMap(map);
     addMarkers(map);
 }
 
 function addMarkers(map){
+    var image = {
+        url: "../afbeeldingen/icon_stadion.png",
+        scaledSize: new google.maps.Size(40,40)
+    };
     for(var i =0 ; i < stadions.length; i++){
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(stadions[i].lat, stadions[i].long),
-            animation: google.maps.Animation.bounce
+        let stadion = stadions[i];
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(stadion.lat, stadion.long),
+            animation: google.maps.Animation.bounce,
+            icon: image
         });
         marker.setMap(map);
+        marker.addListener("click", function() {
+            OpenDialog(stadion)
+        });
     }
+}
+
+function OpenDialog(stadion){
+    var dialog = document.getElementById("dialog");
+    dialog.showModal();
+    document.getElementById("title").innerText = stadion.name;
+    document.getElementById("image").src = stadion.img;
+    document.getElementById("locatie").innerText = stadion.city;
+    document.getElementById("capaciteit").innerText = stadion.cap;
+    document.getElementById("sluiten").addEventListener("click",function() {
+        dialog.close();
+    });
+
 }
